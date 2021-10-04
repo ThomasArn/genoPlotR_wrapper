0322f70d7c920dd9dd652533f2473a03642d9bb8
ThomasArn <Thomasarnhansen@gmail.com>
2021-10-04 13:45:17 +0200

    Update recomb_genoplotR_wrapper.sh

--- recomb_genoplotR_wrapper.sh
+++ recomb_genoplotR_wrapper.sh
@@ -8,20 +8,20 @@ sed -e 's/[\!\@\#\\$\%\^\&\*\(\)\|:;.,]/_/g' -e 's/-/_/g' -e 's/"_+"/_/g' $1 > m
 mafft --thread 18 --adjustdirection mafft_genoplotr/input.fa > mafft_genoplotr/mafft_output.fa
 sed -i "s/_R_//g"  mafft_genoplotr/mafft_output.fa
 cd mafft_genoplotr/
-perl /data/data5/Projects/thomas/scripts/convert_fasta2phylip.pl mafft_output.fa
+perl convert_fasta2phylip.pl mafft_output.fa
 
 raxmlHPC-PTHREADS-AVX -f a -m GTRGAMMA -p 12345 -x 12345 -# 100 -T 18 -s mafft_output.fa.phylip -n recomb_mafft.raxml 
 
 ####################### split up multi fasta file #######################
 mkdir -p PROKKA
-python /data/data5/Projects/thomas/scripts/python_multiple_alignment_genopltR.py mafft_output.fa
+python python_multiple_alignment_genopltR.py mafft_output.fa
 
 
 ########### Run prokka ######################
 mkdir -p PROKKA/prokka_out
 for f in `ls PROKKA/*fa`; do
   bsname=`basename $f .fa`
-  nice -n 19 /data/data5/Projects/thomas/bin/prokka/prokka-1.11/bin/prokka --force --genus Staphylococcus --species "not applicable" -strain $bsname --outdir PROKKA/prokka_out/prokka_$bsname --prefix $bsname $f --locustag $bsname --addgenes
+  nice -n 19 prokka --force --genus Staphylococcus --species "not applicable" -strain $bsname --outdir PROKKA/prokka_out/prokka_$bsname --prefix $bsname $f --locustag $bsname --addgenes
 done 
 
 ########### Do blast combinations ######################
@@ -41,7 +41,7 @@ for i in `cat FileOfFiles.txt`; do
   done
 done 
 
-Rscript /data/data5/Projects/thomas/scripts/Rscript_genoplotR_wrapper.R PROKKA/prokka_out/ RAxML_bipartitions.recomb_mafft.raxml 
+Rscript Rscript_genoplotR_wrapper.R PROKKA/prokka_out/ RAxML_bipartitions.recomb_mafft.raxml 
 
 cd ..
 rm -r mafft_genoplotr/
